011103b997a7c938c113064e51df36128c5328b7
Trevor Clark <jtclark2@gmail.com>
2021-08-13 16:12:02 +0000

    Adding azure version.

--- hello.py
+++ hello.py
@@ -3,4 +3,9 @@ def add(x, y):
 
 
 result = add(1, 2)
-print(f"This is the sum: {1}, {1}, {result}")
+
+# removing formatted strings to remain consistent with tutorial
+# They don't work in python <3.6, and azure was still on python3.5 when tutorial was made  
+# not sure why we don't just upgrade python environment instead? Maybe we will shortly.
+# print(f"This is the sum: {1}, {1}, {result}")
+print("This is the sum: %d, %d, %d" % (1, 1, result))
